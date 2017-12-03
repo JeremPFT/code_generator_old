bc4fa5ce6e47fe9af847cd7a55c47945de680c26
Jeremy Piffret <j.piffret@gmail.com>
2017-12-03 23:44:47 +0100

    modification des renommages / use

--- src/exec/exec.adb
+++ src/exec/exec.adb
@@ -1,7 +1,6 @@
 with String_Utils;
 
 with Project_Loader;
-use Project_Loader;
 
 with Generator;
 
@@ -12,78 +11,86 @@ procedure Exec is
     return String_Utils.String_Access
     is (new String'(Item));
 
-  package Load renames Project_Loader;
-
 begin
 
-  Load.Project
-    (Path         => "/tmp",
-     Project_Name => "test_gen_code_generator");
+  Load_Project :
+  declare
+    use Project_Loader;
+  begin
+    Project
+      (Path         => "/tmp",
+       Project_Name => "test_gen_code_generator");
 
-  --  Console_Project
-  --    ("/home/jeremy/workspace/ada/" &
-  --       "test_gen_code_generator");
+    --  Console_Project
+    --    ("/home/jeremy/workspace/ada/" &
+    --       "test_gen_code_generator");
 
-  Load.Module
-    ("model");
+    Module
+      ("model");
 
-  Load.Class
-    ("element; abstract, create");
-  Load.Field
-    ("owned_comment : vector := empty; " &
-       "treat_as_string, add, has, number");
-  Load.Method
-    ("visit (object : in out visitor.object_t'class)");
+    Class
+      ("element; abstract, create");
+    Field
+      ("owned_comment : vector; " &
+         "treat_as_string, add, has, number");
+    Method
+      ("visit (object : in out visitor.object_t'class)");
 
-  Load.Class
-    ("named_element : element and test and chose; abstract, init, create");
-  Load.Field
-    ("name : access string := null; treat_as_string, get, create");
+    Class
+      ("named_element : element and test and chose; abstract, init, create");
+    Field
+      ("name : access string := null; treat_as_string, get, create");
 
-  Load.Class
-    ("packageable_element " &
-       "(owner_package : access package_def.object_t'class)" &
-       " : named_element; abstract");
+    Class
+      ("packageable_element " &
+         "(owner_package : access package_def.object_t'class)" &
+         " : named_element; abstract");
 
-  Load.Class
-    ("subprogram : packageable_element");
-  Load.Fields
-    ((+"of_type : access string := null",
-      +"is_abstract : boolean := false",
-      +"is_separate : boolean := false"));
-  Load.Method
-    ("has_return");
+    Class
+      ("subprogram : packageable_element");
+    Fields
+      ((+"of_type : access string := null",
+        +"is_abstract : boolean := false",
+        +"is_separate : boolean := false"));
+    Method
+      ("has_return");
 
-  Load.Class
-    ("type_def : packageable_element; abstract");
+    Class
+      ("type_def : packageable_element; abstract");
 
-  Load.Class
-    ("interface_def : type_def");
-  Load.Fields
-    ((+("parent_interfaces : vector := empty, " &
-          "has, number, get_i, add "),
-      +("Public_Subprograms : subprogram.vector := empty, " &
-          "has, number, get_i, get, add")));
+    Class
+      ("interface_def : type_def");
+    Fields
+      ((+("parent_interfaces : vector, " &
+            "has, number, get_i, add "),
+        +("Public_Subprograms : subprogram.vector, " &
+            "has, number, get_i, get, add")));
 
-  Load.Class
-    ("class_def : interface_def");
-  Load.Fields
-    ((+("parent_interfaces : vector := empty, " &
-          "has, number, get_i, add "),
-      +("Subprograms : subprogram.vector := empty, " &
-          "has, number, get_i, get, add")));
+    Class
+      ("class_def : interface_def");
+    Fields
+      ((+("parent_interfaces : vector, " &
+            "has, number, get_i, add "),
+        +("Subprograms : subprogram.vector, " &
+            "has, number, get_i, get, add")));
 
-  Load.Module
-    ("exec");
+    Module
+      ("exec");
 
-  Load.Module
-    ("ada_utils");
+    Module
+      ("ada_utils");
 
-  Load.Module
-    ("file_io");
+    Module
+      ("file_io");
 
-  Load.Close;
+    Close;
+  end Load_Project;
 
-  Generator.Generate (Load.Get_Data);
+  Generate_Project :
+  declare
+    use Generator;
+  begin
+    Generate (Project_Loader.Get_Data);
+  end Generate_Project;
 
 end Exec;
